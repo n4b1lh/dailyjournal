b8161ac30961b70c0b13e0f3d7a7d55406e6c1bc
n4b1lh <nabilhaidar43@gmail.com>
2026-01-10 22:46:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,247 @@
+<?php
+//menyertakan code dari file koneksi
+include "koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Latihan Bootstrap Nabil</title>
+    <link rel="icon" type="image/x-icon" href="img/logo.png" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <!-- Nav begin -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Latihan Bootstrap Nabil</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-dark">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#article">Artikel</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#gallery">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php" target="_blank">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Nav end -->
+
+    <!-- hero begin -->
+    <section id="hero" class="text-center p-5 bg-danger-subtle text-sm-start">
+      <div class="container">
+        <div class="d-sm-flex flex-sm-row-reverse align-items-center">
+          <img src="img/banner.jpg" class="img-fluid" width="300" />
+          <div>
+            <h1 class="fw-bold display-4">
+              Create Memories, Save Memories, Everyday
+            </h1>
+            <h4 class="lead display-6">
+              Mencatat semua kegiatan sehari-hari yang ada tanpa terkecuali
+            </h4>
+            <span id="tanggal"></span>
+            <span id="jam"></span>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- hero end -->
+
+    <!-- article begin  -->
+    <section id="article" class="text-center p-5">
+      <div class="container">
+        <h1 class="fw-bold display-4 pb-3">Artikel</h1>
+        <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+        <?php
+        $sql = "SELECT * FROM article ORDER BY tanggal DESC";
+        $hasil = $conn->query($sql); 
+
+        while ($row = $hasil->fetch_assoc()) {
+        ?>
+            <!-- col begin -->
+          <div class="col">
+            <div class="card h-100">
+              <img
+                src="img/<?php echo $row["gambar"]; ?>"
+                class="card-img-top"
+                alt="archictecture1"
+              />
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $row["judul"]; ?></h5>
+                <p class="card-text">
+                    <?= $row["isi"]?>
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-body-secondary">
+                    <?= $row["tanggal"]?>
+                </small>
+              </div>
+            </div>
+          </div>
+          <!-- col end -->
+           <?php
+        }
+        ?>
+      </div>
+    </div>
+    </section>
+    <!-- article end -->
+
+    <!-- gallery begin -->
+    <section id="gallery" class="text-center p-5 bg-danger-subtle">
+      <div class="container">
+        <h1 class="fw-bold display-4 pb-3">Gallery</h1>
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="img/gallery1.jpg" class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src="img/gallery2.jpg" class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src="img/gallery3.jpg" class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src="img/gallery4.jpg" class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src="img/gallery5.jpg" class="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </section>
+    <!-- gallery end -->
+
+    <!-- footer begin -->
+    <footer class="text-center p-5">
+      <div>
+        <a href="https://www.instagram.com/udinusofficial/"
+          ><i class="bi bi-instagram h2 p-2 text-dark"></i
+        ></a>
+        <a href="https://x.com/udinusofficial"
+          ><i class="bi bi-twitter-x h2 p-2 text-dark"></i
+        ></a>
+        <a href="https://wa.me/6281234567890"
+          ><i class="bi bi-whatsapp h2 p-2 text-dark"></i
+        ></a>
+      </div>
+      <div>Muhammad Nabil Haidar A11.2024.15584 &copy; 2025</div>
+    </footer>
+    <!-- footer end -->
+    <!-- Tombol Back to Top -->
+    <button
+      id="backToTop"
+      class="btn btn-danger rounded-circle position-fixed bottom-0 end-0 m-3 d-none"
+    >
+      <i class="bi bi-arrow-up" title="Back to Top"></i>
+    </button>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+      crossorigin="anonymous"
+    ></script>
+    <script type="text/javascript">
+      function tampilWaktu() {
+        const waktu = new Date();
+
+        const tanggal = waktu.getDate();
+        const bulan = waktu.getMonth();
+        const tahun = waktu.getFullYear();
+        const jam = waktu.getHours();
+        const menit = waktu.getMinutes();
+        const detik = waktu.getSeconds();
+
+        const arrBulan = [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+        ];
+
+        const tanggal_full = tanggal + "/" + arrBulan[bulan] + "/" + tahun;
+        const jam_full = jam + ":" + menit + ":" + detik;
+
+        document.getElementById("tanggal").innerHTML = tanggal_full;
+        document.getElementById("jam").innerHTML = jam_full;
+      }
+      setInterval(tampilWaktu, 1000);
+    </script>
+
+    <script type="text/javascript">
+      const backToTop = document.getElementById("backToTop");
+
+      window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+          backToTop.classList.remove("d-none");
+          backToTop.classList.add("d-block");
+        } else {
+          backToTop.classList.remove("d-block");
+          backToTop.classList.add("d-none");
+        }
+      });
+
+      backToTop.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      });
+    </script>
+  </body>
+</html>
